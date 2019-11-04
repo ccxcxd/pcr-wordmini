964c8150550a050fca06c3f23ad16ed31c38167f
CCX_CX_D <xx.chen@live.com>
2019-11-03 19:58:31 -0800

    change table to plan texts

--- js/main.js
+++ js/main.js
@@ -2,29 +2,30 @@
     var char_data = JSON.parse($('#char_data').text());
     var word_data = JSON.parse($('#word_data').text());
     
-    var pre_col = 9999;
-    var row_obj = null;
+    var pre_col = 1;
     $.each(char_data, function (char_id, data) {
         if (data.col < pre_col) {
-            if (row_obj != null) row_obj.appendTo("#char_table");
-            row_obj = $("<tr>");
+            $("#char_table").append("<br/>");
             pre_col = 1;
         } else {
             pre_col++;
         }
         // deal with have empty col
         while (pre_col < data.col) {
-            row_obj.append($("<td>"));
+            $("#char_table").append($("<button>")
+                .width(100)
+                .addClass("btn btn-light")
+            );
             pre_col++;
         }
         var display_text = data.hira + '/' + data.kata + '/' + data.roma;
-        var button = $("<button/>")
+        $("#char_table").append($("<button>")
             .text(display_text)
+            .width(100)
             .addClass("btn btn-primary")
-            .click(function() {popup(char_id, word_data);});
-        row_obj.append($("<td>").append(button));
+            .click(function() {popup(char_id, word_data);})
+        );
     });
-    if (row_obj != null) row_obj.appendTo("#char_table");
 });
 
 function popup(char_id, word_data) {
@@ -35,5 +36,5 @@ function popup(char_id, word_data) {
             $("#img_holder").append('<img src="img/' + data.pic + '.png"/>');
         }
     });
-    $('#img_modal').modal('show');
+    $("#img_modal").modal("show");
 }
