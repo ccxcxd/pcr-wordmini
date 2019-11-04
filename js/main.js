7ef4b2753de6cd779cb672291e8f449a35c99925
CCX_CX_D <xx.chen@live.com>
2019-11-03 18:39:07 -0800

    first version

---
+++ js/main.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    var char_data = JSON.parse($('#char_data').text());
+    var word_data = JSON.parse($('#word_data').text());
+    
+    var pre_col = 9999;
+    var row_obj = null;
+    $.each(char_data, function (char_id, data) {
+        if (data.col < pre_col) {
+            if (row_obj != null) row_obj.appendTo("#char_table");
+            row_obj = $("<tr>");
+            pre_col = 1;
+        } else {
+            pre_col++;
+        }
+        // deal with have empty col
+        while (pre_col < data.col) {
+            row_obj.append($("<td>"));
+            pre_col++;
+        }
+        var display_text = data.hira + '/' + data.kata + '/' + data.roma;
+        var button = $("<button/>")
+            .text(display_text)
+            .addClass("btn btn-primary")
+            .click(function() {popup(char_id, word_data);});
+        row_obj.append($("<td>").append(button));
+    });
+    if (row_obj != null) row_obj.appendTo("#char_table");
+});
+
+function popup(char_id, word_data) {
+    $("#img_title").text(char_id);
+    $("#img_holder").empty();
+    $.each(word_data, function (word_id, data) {
+        if (data.first === char_id) {
+            $("#img_holder").append('<img src="img/' + data.pic + '.png"/>');
+        }
+    });
+    $('#img_modal').modal('show');
+}
